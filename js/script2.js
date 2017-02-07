6cdf73456c85536943e5428b596c58e783602d1b
SakshiPratap12 <sakshi.2392@gmail.com>
2017-02-07 10:58:03 -0500

    Changes

--- js/script2.js
+++ js/script2.js
@@ -1,5 +1,54 @@
 
 var teams;
+function teamNameChanges(team,year){
+    var changedTeams = ['ram','crd','clt','sdg','nwe','rai','oti']
+
+    if(changedTeams.indexOf(team)<0)
+        return ""
+    else if(team =='crd'){
+        if(year<=1987)
+            return "(St. Louis Cardinals)"
+
+        if(year>=1988 && year<1994)
+            return "(Phoenix Cardinals)"
+
+    }
+
+    else if(team =='ram'){
+        if(year>=1995 && year<=2015)
+            return "(St. Louis Rams)"
+    }
+
+    else if(team =='clt'){
+        if(year<1984 )
+            return "(Baltimore Colts)"
+    }
+
+
+    else if(team =='sdg'){
+        if(year<2016 )
+            return "(San Diego Chargers)"
+    }
+
+    else if(team =='nwe'){
+        if(year<=1970 )
+            return "(Boston Patriots)"
+    }
+
+    else if(team =='rai'){
+        if(year>=1982 && year<1995)
+            return "(Los Angeles Raiders)"
+    }
+
+    else if(team =='oti'){
+        if(year<1997)
+            return "(Houston Oilers)"
+    }
+
+    return ""
+
+}
+
 
 $(function(){
 var widthScreen = '100%'
@@ -79,20 +128,20 @@ function highlightTeam(){
 }
 
 function unselectAll(){
-    d3.selectAll(".circleGroup circle")
+    d3.selectAll(".circleGroup rect")
               .classed("unhighlight",false)
-      d3.selectAll(".circleGroup circle")
+      d3.selectAll(".circleGroup rect")
       .classed("highlight",false)
 
 
-     d3.selectAll(".circleGroup circle")
-              .classed("unclicked",false)
-      d3.selectAll(".circleGroup circle")
+     d3.selectAll(".circleGroup rect")
+              .classed("clicked",false)
+      d3.selectAll(".circleGroup rect")
       .classed("unclicked",false)
 
-     d3.selectAll(".circleGroup circle")
+     d3.selectAll(".circleGroup rect")
               .classed("LogoClicked",false)
-      d3.selectAll(".circleGroup circle")
+      d3.selectAll(".circleGroup rect")
       .classed("unLogoClicked",false)
 
        d3.selectAll(".highlight")
@@ -260,14 +309,17 @@ function createChart(allData,team,selector) {
                    .style("opacity", .9);
 
               tooltip.html(function(){
+                   teamActualName = teamNameChanges(d.oppn,d.year)
                    score = allData.results[d.oppn][d.year].won
                    score_opp = allData.results[d.oppn][d.year].lost
+                   home=""
                    won = d["won_flag"]?" (W) ":" (L)"
                    if (d["team_score"] ==d["oppn_score"])
                         won=" (T)"
-                   home =d.home_flag?" (Home)":" (Away)"
+                   if(d.week<18)
+                        home =d.home_flag?" (Away)":" (Home)"
                    str ="Date: "+dateFormat(d["date"])+"&nbsp"+home+"<br/>"+
-                     "Vs "+teams[d["oppn"]].name + "<br/>"+
+                     "Vs "+teams[d["oppn"]].name +teamActualName+"<br/>"+
                     "Score: "+d["team_score"]+"-" +d["oppn_score"] +won+"<br/>"+
                     "Opponent record: "+score+"-" +score_opp +"<br/>"
                     return str
@@ -462,7 +514,7 @@ function createChart(allData,team,selector) {
                             return "win1";
                         }
                     }else if(d.team_score== d.oppn_score){
-                          return "yetToPlay"
+                          return "draw"
 
                     }else{
                         if(scoreDiff<=10){
